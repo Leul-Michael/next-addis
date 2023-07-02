0ac85b5fad54b39abe914ebf710f97ebae4c0676
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-07-02 13:15:10 +0300

    first commit

--- src/components/hero/Services.tsx
+++ src/components/hero/Services.tsx
@@ -4,7 +4,7 @@ import ServiceExcerpt from "../excerpts/ServiceExcerpt"
 
 const Services = () => {
   return (
-    <section className="flex flex-col gap-16 py-20">
+    <section className="flex flex-col gap-24 py-20">
       <div className="flex items-start justify-between gap-16">
         <p className="max-w-[400px] text-lg leading-[1.2] text-clr-muted">
           Never let a bad design decesions ruin your business. We are here
@@ -12,11 +12,11 @@ const Services = () => {
         </p>
         <div className="flex flex-col gap-14">
           <p className="max-w-[650px] text-4xl leading-[1.2] text-clr-muted">
-            We are a{" "}
-            <span className="font-medium underline">creative agency</span>{" "}
-            working with brands - building insightful strategy, creating unique
-            designs and crafting experiences that bring positive change and
-            value.
+            We unite design, technology and the brightest ideas to deliver
+            sustainable, sharp,{" "}
+            <span className="font-medium underline">aesthetic</span> and{" "}
+            <span className="font-medium underline">high-functioning</span>{" "}
+            products.
           </p>
         </div>
       </div>
