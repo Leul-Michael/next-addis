4782b28de8d0db3e7679ee35e06051f9a3762466
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-07-04 14:49:28 +0300

    first commit

--- src/components/excerpts/TechnologyExcerpt.tsx
+++ src/components/excerpts/TechnologyExcerpt.tsx
@@ -8,25 +8,20 @@ type TechnologyExcerptProps = {
 const TechnologyExcerpt = ({ name, image }: TechnologyExcerptProps) => {
   return (
     <article
-      className={`flex h-full flex-col items-center justify-center overflow-hidden
+      className={`flex h-full max-w-[500px] flex-col items-center justify-center gap-8 overflow-hidden
         rounded-[6px] bg-clr-border/30 px-5 py-3`}
     >
-      <div
-        className="flex max-w-[500px] flex-col items-center justify-center gap-5
-        rounded-[6px]"
-      >
-        <h1 className="max-w-[400px] text-base font-semibold leading-none md:text-xl">
-          {name}
-        </h1>
-        <div className="relative aspect-square w-20 overflow-hidden rounded-[4px] lg:w-24">
-          <Image
-            src={image}
-            alt="technology image"
-            fill
-            sizes="(max-width: 0px) 100vw"
-            className="absolute left-0 top-0 h-full w-full object-cover"
-          />
-        </div>
+      <h1 className="max-w-[400px] text-center text-base font-semibold leading-none md:text-xl">
+        {name}
+      </h1>
+      <div className="relative aspect-square w-20 overflow-hidden rounded-[4px] lg:w-24">
+        <Image
+          src={image}
+          alt="technology image"
+          fill
+          sizes="(max-width: 0px) 100vw"
+          className="absolute left-0 top-0 h-full w-full object-cover"
+        />
       </div>
     </article>
   )
