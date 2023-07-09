a53ad6be4541e041e4bc5c3f68d08ea1e6693cca
Leul Michael <109846940+Leul-Michael@users.noreply.github.com>
2023-07-09 11:57:27 +0300

    first commit

--- src/components/excerpts/ServiceMobileExcerpt.tsx
+++ src/components/excerpts/ServiceMobileExcerpt.tsx
@@ -1,5 +1,7 @@
 import { FiArrowUpRight } from "react-icons/fi"
 import * as Icons from "react-icons/ai"
+import { BsArrowDown } from "react-icons/bs"
+import Link from "next/link"
 
 type ServiceMobileExcerptProps = {
   name: string
@@ -9,22 +11,21 @@ type ServiceMobileExcerptProps = {
 const ServiceMobileExcerpt = ({ name, icon }: ServiceMobileExcerptProps) => {
   const Icon = (Icons as any)[icon]
   return (
-    <article
-      className={`flex h-full flex-col justify-center overflow-hidden border-y border-clr-border py-4`}
+    <Link
+      href="/service/lools"
+      className={`group relative flex h-full flex-col justify-center overflow-hidden border-y border-clr-border py-4 hover:text-[#151515]`}
     >
+      <span className="custom-transition absolute bottom-0 left-0 -z-[1] h-0 w-full bg-clr-purple duration-300 group-hover:h-full"></span>
       <div
         className={`flex h-full w-full flex-col rounded-[4px] p-4 md:p-8 lg:p-12`}
       >
         <div className="grid w-full grid-cols-[40px_1fr_40px] items-start gap-2 md:gap-4 lg:gap-8">
           <Icon size={25} className="duration-300 group-hover:-rotate-45" />
           <h1 className="text-[4.5vw] font-semibold leading-none">{name}</h1>
-          <FiArrowUpRight
-            size={25}
-            className="duration-300 group-hover:-rotate-45"
-          />
+          <BsArrowDown size={25} className="-rotate-[135deg] duration-300" />
         </div>
       </div>
-    </article>
+    </Link>
   )
 }
 
